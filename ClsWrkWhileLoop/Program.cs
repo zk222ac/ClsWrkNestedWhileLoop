2fbb189c969889cc53fcfec7a2f937a6a57a7864
Zuhair Haroon Khan <zk222ac@gmail.com>
2018-10-02 09:06:49 +0200

    final release

--- ClsWrkWhileLoop/Program.cs
+++ ClsWrkWhileLoop/Program.cs
@@ -10,6 +10,36 @@ namespace ClsWrkWhileLoop
     {
         static void Main(string[] args)
         {
+            // how to declare the while loop 
+
+            // initialize counter
+            int i = 0;
+
+            // define while loop
+            // outer loop
+            while (i < 10)
+            {
+                Console.WriteLine("Outer loop while loop .....");
+                // inner while loop
+                int j = 0;
+                while (j < 5)
+                {
+                    int k = 0;
+                    while (k < 3)
+                    {
+                        Console.WriteLine(" loop inside inner loop...");
+                        k++;
+                    }
+                    Console.WriteLine("Inner while loop....... ");
+                    j++;
+                }
+
+                // incremental counter
+                i++;
+            }
+
+            Console.ReadKey();
+
         }
     }
 }
